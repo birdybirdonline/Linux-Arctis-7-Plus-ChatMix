880a576eb7b3e10bf158039be315fd25692d1e03
Giacomo 'Mr. Wolf' Furlan <giacomo.furlan@fastwebnet.it>
2024-12-25 23:33:07 +0100

    Adapt AllSound_Arctis_ChatMix.py (optimization work can be largely done)

--- AllSound_Arctis_ChatMix.py
+++ AllSound_Arctis_ChatMix.py
@@ -19,41 +19,54 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
     """
 
-# Ver info
-""" AllSound version, which allows the chatmix dial on the headset
-    to control the system default device, no matter which one
-    """ 
-
 import os
 import sys
 import signal
 import logging
+
 import usb.core
 
+from arctis_devices.config import udev_devices
 
-class Arctis7PlusChatMix:
+# Ver info
+""" AllSound version, which allows the chatmix dial on the headset
+    to control the system default device, no matter which one
+    """
+
+
+class ArctisAllSoundChatMix:
     def __init__(self):
 
         # set to receive signal from systemd for termination
         signal.signal(signal.SIGTERM, self.__handle_sigterm)
 
         self.log = self._init_log()
-        self.log.info("Initializing ac7pcm...")
+        self.log.info("Initializing ac-pcm...")
 
-        # identify the arctis 7+ device
-        try:
-            self.dev=usb.core.find(idVendor=0x1038, idProduct=0x220e)
-        except Exception as e:
-            self.log.error("""Failed to identify the Arctis 7+ device.
+        # identify the arctis device
+        for device in udev_devices:
+            try:
+                self.dev = usb.core.find(idVendor=device.vendor_id, idProduct=device.product_id)
+                if self.dev is None:
+                    continue
+                self.udev_device = device
+
+                self.log.info(f'Found device {device.name}')
+
+                break
+            except Exception as e:
+                pass
+
+        if self.dev is None:
+            self.log.error(f"""Failed to identify the Arctis device.
             Please ensure it is connected.\n
-            Please note: This program only supports the '7+' model.""")
-            self.die_gracefully(trigger)
+            Please note: This program supports the following models: {', '.join(udev_devices.keys())}.""")
+            self.die_gracefully(trigger="Couldn't find any Arctis model")
 
         # select its interface and USB endpoint, and capture the endpoint address
         try:
-            # interface index 7 of the Arctis 7+ is the USB HID for the ChatMix dial;
-            # its actual interface number on the device itself is 5.
-            self.interface = self.dev[0].interfaces()[7]
+            # interface index of the USB HID for the ChatMix dial might differ from the interface number on the device itself
+            self.interface: usb.core.Interface = self.dev[0].interfaces()[self.udev_device.chatmix_hid_interface]
             self.interface_num = self.interface.bInterfaceNumber
             self.endpoint = self.interface.endpoints()[0]
             self.addr = self.endpoint.bEndpointAddress
@@ -61,7 +74,7 @@ class Arctis7PlusChatMix:
         except Exception as e:
             self.log.error("""Failure to identify relevant 
             USB device's interface or endpoint. Shutting down...""")
-            self.die_gracefully(exc=True, trigger ="identification of USB endpoint")
+            self.die_gracefully(exc=True, trigger="identification of USB endpoint")
 
         # detach if the device is active
         if self.dev.is_kernel_driver_active(self.interface_num):
@@ -69,14 +82,13 @@ class Arctis7PlusChatMix:
 
         self.VAC = self._init_VAC()
 
-
     def _init_log(self):
         log = logging.getLogger(__name__)
         log.setLevel(logging.DEBUG)
         stdout_handler = logging.StreamHandler()
         stdout_handler.setLevel(logging.DEBUG)
         stdout_handler.setFormatter(logging.Formatter('%(levelname)8s | %(message)s'))
-        log.addHandler(stdout_handler)    
+        log.addHandler(stdout_handler)
         return (log)
 
     def _init_VAC(self):
@@ -87,29 +99,6 @@ class Arctis7PlusChatMix:
         # get the default sink id from pactl
         self.system_default_sink = os.popen("pactl get-default-sink").read().strip()
         self.log.info(f"default sink identified as {self.system_default_sink}")
-
-        # # attempt to identify an Arctis sink via pactl
-        # try:
-        #     pactl_short_sinks = os.popen("pactl list short sinks").readlines()
-        #     # grab any elements from list of pactl sinks that are Arctis 7
-        #     arctis = re.compile('.*[aA]rctis.*7')
-        #     arctis_sink = list(filter(arctis.match, pactl_short_sinks))[0] 
-
-        #     # split the arctis line on tabs (which form table given by 'pactl short sinks')
-        #     tabs_pattern = re.compile(r'\t')
-        #     tabs_re = re.split(tabs_pattern, arctis_sink)
-
-        #     # skip first element of tabs_re (sink's ID which is not persistent)
-        #     arctis_device = tabs_re[1]
-        #     self.log.info(f"Arctis sink identified as {arctis_device}")
-        #     default_sink = arctis_device
-
-        # except Exception as e:
-        #     self.log.error("""Something wrong with Arctis definition 
-        #     in pactl list short sinks regex matching.
-        #     Likely no match found for device, check traceback.
-        #     """, exc_info=True)
-        #     self.die_gracefully(trigger="No Arctis device match")
 
         # Destroy virtual sinks if they already existed incase of previous failure:
         try:
@@ -123,61 +112,62 @@ class Arctis7PlusChatMix:
         # Instantiate our virtual sinks - Arctis_Chat and Arctis_Game
         try:
             self.log.info("Creating VACS...")
-            os.system("""pw-cli create-node adapter '{ 
-                factory.name=support.null-audio-sink 
-                node.name=ChatMix_Game 
-                node.description="ChatMix Game" 
-                media.class=Audio/Sink 
-                monitor.channel-volumes=true 
-                object.linger=true 
-                audio.position=[FL FR]
-                }' 1>/dev/null
+            os.system(f"""pw-cli create-node adapter '{{
+                factory.name=support.null-audio-sink
+                node.name=ChatMix_Game
+                node.description="ChatMix Game"
+                media.class=Audio/Sink
+                monitor.channel-volumes=true
+                object.linger=true
+                audio.position=[{' '.join(self.udev_device.audio_position)}]
+                }}' 1>/dev/null
             """)
 
-            os.system("""pw-cli create-node adapter '{ 
-                factory.name=support.null-audio-sink 
-                node.name=ChatMix_Chat 
-                node.description="ChatMix Chat" 
-                media.class=Audio/Sink 
-                monitor.channel-volumes=true 
-                object.linger=true 
-                audio.position=[FL FR]
-                }' 1>/dev/null
+            os.system(f"""pw-cli create-node adapter '{{
+                factory.name=support.null-audio-sink
+                node.name=ChatMix_Chat
+                node.description="ChatMix Chat"
+                media.class=Audio/Sink
+                monitor.channel-volumes=true
+                object.linger=true
+                audio.position=[{' '.join(self.udev_device.audio_position)}]
+                }}' 1>/dev/null
             """)
         except Exception as E:
             self.log.error("""Failure to create node adapter - 
             ChatMix virtual device could not be created""", exc_info=True)
             self.die_gracefully(sink_creation_fail=True, trigger="VAC node adapter")
 
-        #route the virtual sink's L&R channels to the default system output's LR
+        # route the virtual sink's L&R channels to the default system output's LR
         try:
             self.log.info("Assigning VAC sink monitors output to default device...")
 
             os.system(f'pw-link "ChatMix_Game:monitor_FL" '
-            f'"{default_sink}:playback_FL" 1>/dev/null')
+                      f'"{self.system_default_sink}:playback_FL" 1>/dev/null')
 
             os.system(f'pw-link "ChatMix_Game:monitor_FR" '
-            f'"{default_sink}:playback_FR" 1>/dev/null')
+                      f'"{self.system_default_sink}:playback_FR" 1>/dev/null')
 
             os.system(f'pw-link "ChatMix_Chat:monitor_FL" '
-            f'"{default_sink}:playback_FL" 1>/dev/null')
+                      f'"{self.system_default_sink}:playback_FL" 1>/dev/null')
 
             os.system(f'pw-link "ChatMix_Chat:monitor_FR" '
-            f'"{default_sink}:playback_FR" 1>/dev/null')
+                      f'"{self.system_default_sink}:playback_FR" 1>/dev/null')
 
         except Exception as e:
             self.log.error("""Couldn't create the links to 
             pipe LR from VAC to default device""", exc_info=True)
             self.die_gracefully(sink_fail=True, trigger="LR links")
-        
-        # set the default sink to Arctis Game
-        # os.system('pactl set-default-sink Arctis_Game')
 
     def start_modulator_signal(self):
         """Listen to the USB device for modulator knob's signal 
         and adjust volume accordingly
         """
-        
+
+        if self.udev_device.device_initializer is not None:
+            self.log.info('Initializing device...')
+            self.udev_device.device_initializer(self.dev, self.log)
+
         self.log.info("Reading modulator USB input started")
         self.log.info("-"*45)
         self.log.info("ChatMix Enabled!")
@@ -188,9 +178,11 @@ class Arctis7PlusChatMix:
                 # read_input[1] returns value to use for default device volume
                 # read_input[2] returns the value to use for virtual device volume
                 read_input = self.dev.read(self.addr, 64)
-                default_device_volume = "{}%".format(read_input[1])
-                virtual_device_volume = "{}%".format(read_input[2])
-        
+                volume_data = self.udev_device.read_volume_data(read_input)
+
+                default_device_volume = "{}%".format(volume_data[0])
+                virtual_device_volume = "{}%".format(volume_data[1])
+
                 # os.system calls to issue the commands directly to pactl
                 os.system(f'pactl set-sink-volume ChatMix_Game {default_device_volume}')
                 os.system(f'pactl set-sink-volume ChatMix_Chat {virtual_device_volume}')
@@ -207,12 +199,12 @@ class Arctis7PlusChatMix:
         """Kill the process and remove the VACs
         on fatal exceptions or SIGTERM / SIGINT
         """
-        
+
         self.log.info('Cleanup on shutdown')
         # os.system(f"pactl set-default-sink {self.system_default_sink}")
 
         # cleanup virtual sinks if they exist
-        if  sink_creation_fail == False:
+        if sink_creation_fail == False:
             self.log.info("Destroying virtual sinks...")
             os.system("pw-cli destroy ChatMix_Game 1>/dev/null")
             os.system("pw-cli destroy ChatMix_Chat 1>/dev/null")
@@ -228,7 +220,8 @@ class Arctis7PlusChatMix:
             self.log.info("-"*45)
             sys.exit(0)
 
+
 # init
 if __name__ == '__main__':
-    a7pcm_service = Arctis7PlusChatMix()
-    a7pcm_service.start_modulator_signal()
+    arctis_pcm_service = ArctisAllSoundChatMix()
+    arctis_pcm_service.start_modulator_signal()
