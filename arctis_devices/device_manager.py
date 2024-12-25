e21ba53551ddd7fc1d2dac9c9bc21dcbe645f701
Giacomo 'Mr. Wolf' Furlan <giacomo.furlan@fastwebnet.it>
2024-12-25 23:12:13 +0100

    Added manage_chatmix_input_data to DeviceManager

--- arctis_devices/device_manager.py
+++ arctis_devices/device_manager.py
@@ -8,3 +8,7 @@ class DeviceManager(ABC):
     @abstractmethod
     def get_udev_device(self) -> UdevDevice:
         pass
+
+    @abstractmethod
+    def manage_chatmix_input_data(data: list[int]) -> tuple[int, int]:
+        pass
